aed34bf521b8878248c51f4be102672a921166f8
fkxianzhou <368684850@qq.com>
2023-11-30 17:10:38 +0800

    3次测试

--- app/src/main/java/com/example/myphoto/MainActivity.java
+++ app/src/main/java/com/example/myphoto/MainActivity.java
@@ -9,7 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //fuck world
-        //你好世界！！！
     }
 }
