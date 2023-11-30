1eea43876df713cb3943db73917e260905bd1237
fkxianzhou <368684850@qq.com>
2023-11-30 17:05:13 +0800

    二次测试

--- app/src/main/java/com/example/myphoto/MainActivity.java
+++ app/src/main/java/com/example/myphoto/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //fuck world
+        //你好世界！！！
     }
 }
